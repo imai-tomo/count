63a4b09092af4b0a790d1f30c50211e1308f40d3
Xcode <xcode@goomailnoMacBook-Pro.local>
2017-08-02 13:27:06 +0900

    Initial Commit

---
+++ Count20170802/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count20170802
+//
+//  Created by Xcode on 2017/08/02.
+//  Copyright © 2017年 Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
