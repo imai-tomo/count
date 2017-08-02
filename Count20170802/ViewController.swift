739d3d148a755f2176752de62d82ef00d023622d
Xcode <xcode@goomailnoMacBook-Pro.local>
2017-08-02 16:11:07 +0900

    CocoaPods経由でSVProgressHUDを追加

--- Count20170802/ViewController.swift
+++ Count20170802/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
     
@@ -43,6 +44,12 @@ class ViewController: UIViewController {
         label.text = String(number)
     }
     
+    @IBAction func showHUD() {
+        SVProgressHUD.show()
+    }
     
+    @IBAction func  dismissHUD() {
+        SVProgressHUD.dismiss()
+    }
     
 }
